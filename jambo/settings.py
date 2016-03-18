e367960ad678308668d0ab661eb6a6e676640228
cjiang1209 <cjiang1209@gmail.com>
2016-03-18 17:37:45 -0500

    Import libraries: jQuery, bootstrap, font-awesome.
    Create the basic page layout.
    
    Create login and logout pages.

--- jambo/settings.py
+++ jambo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'common',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'jambo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ 'jambo/templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
