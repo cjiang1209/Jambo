8ff205cabe13126cf3d9db5c572ee76473d3e7a7
cjiang1209 <cjiang1209@gmail.com>
2017-01-08 15:46:36 -0800

    Popup a modal dialog to add new stages for assignments.
    Grace period end date is synchronized with End date by default.

--- courses/static/courses/script/assignment_management_list.js
+++ courses/static/courses/script/assignment_management_list.js
@@ -1,64 +1,75 @@
-$(document).ready(function () {
-	var tmplNewStage = Handlebars.compile($("#new-stage-template").html());
-	var tmplStage = Handlebars.compile($("#stage-template").html());
-	
-	$('.btn-add-stage').on('click', function () {
+function syncEndDateAndGracePeriodEndDate() {
+	var end_date = $('#inputEndDate').data('DateTimePicker').date();
+	$('#inputGracePeriodEndDate').data('DateTimePicker').date(end_date);
+}
+
+function initializeModelAddStage() {
+	$('#inputStartDate').datetimepicker({
+		format: 'MM/DD/Y HH:mm',
+		sideBySide: true
+	});
+	$('#inputEndDate').datetimepicker({
+		format: 'MM/DD/Y HH:mm',
+		sideBySide: true,
+	});
+	$('#inputGracePeriodEndDate').datetimepicker({
+		format: 'MM/DD/Y HH:mm',
+		sideBySide: true,
+	});
+
+	$('#modalAddStage').on('show.bs.modal', function (evt) {
 		var start_date = new Date();
-		start_date.setDate(start_date.getDate() + 1);
+		start_date.setDate(start_date.getDate());
 		start_date.setHours(23);
 		start_date.setMinutes(59);
 		var end_date = new Date();
-		end_date.setDate(end_date.getDate() + 8);
+		end_date.setDate(end_date.getDate() + 7);
 		end_date.setHours(23);
 		end_date.setMinutes(59);
 		var grace_period_end_date = end_date;
-		
-		var assignment = $(this).parent().find('input.hidden-assignment').val();
-		
-		var context = {
-			assignment: assignment
-		};
-		var html = tmplNewStage(context);
-		var tbody = $(this).parent().parent().find('table.table-stages tbody');
-		tbody.append(html);
-		
-		tbody.find('form.form_new_stage input[name="start_date"]').datetimepicker({
-			format: 'MM/DD/Y HH:mm',
-			defaultDate: start_date
-		});
-		tbody.find('form.form_new_stage input[name="end_date"]').datetimepicker({
-			format: 'MM/DD/Y HH:mm',
-			defaultDate: end_date
-		});
-		tbody.find('form.form_new_stage input[name="grace_period_end_date"]').datetimepicker({
-			format: 'MM/DD/Y HH:mm',
-			defaultDate: grace_period_end_date
-		});
+
+		$('#inputStartDate').data('DateTimePicker').date(start_date);
+		$('#inputEndDate').data('DateTimePicker').date(end_date);
+		$('#inputGracePeriodEndDate').data('DateTimePicker').date(grace_period_end_date);
+
+		$('#checkSyncEndDateAndGracePeriod').prop('checked', true);
+		$('#inputGracePeriodEndDate').prop('readonly', true);
 	});
-	
-	$('.table-stages').on('submit', '.form_new_stage', function (evt) {
+
+	$('#checkSyncEndDateAndGracePeriod').change(function () {
+		if (this.checked) {
+			// Sync
+			$('#inputGracePeriodEndDate').prop('readonly', true);
+			syncEndDateAndGracePeriodEndDate();
+		}
+		else {
+			// No sync
+			$('#inputGracePeriodEndDate').prop('readonly', false);
+		}
+	});
+
+	$('#inputEndDate').on('dp.change', function () {
+		if ($('#checkSyncEndDateAndGracePeriod').prop('checked')) {
+			syncEndDateAndGracePeriodEndDate();
+		}
+	});
+
+	$('#formAddStage').submit(function (evt) {
 		evt.preventDefault();
-		
+
 		var form = $(this);
-		var start_date = form.find('input[name="start_date"]').val();
-		var end_date = form.find('input[name="end_date"]').val();
-		var grace_period_end_date = form.find('input[name="grace_period_end_date"]').val();
-		var assignment = form.find('input[name="assignment"]').val();
-		
+		var start_date = $('#inputStartDate').val();
+		var end_date = $('#inputEndDate').val();
+		var grace_period_end_date = $('#inputGracePeriodEndDate').val();
+		var assignment = $('#inputAssignment').val();
+
 		$.post(urlAddStage, {
 			start_date: start_date,
 			end_date: end_date,
 			grace_period_end_date: grace_period_end_date,
 			assignment: assignment
 		}).done(function (data) {
-			var context = {
-				id: data.id,
-				start_date: start_date,
-				end_date: end_date,
-				grace_period_end_date: grace_period_end_date
-			};
-			var html = tmplStage(context);
-			form.closest('tr').replaceWith(html);
+			location.reload();
 		}).fail(function(response) {
 			$.alert({
 				title: 'Error',
@@ -66,16 +77,19 @@ $(document).ready(function () {
 			});
 		});
 	});
-	
-	$('.table-stages').on('click', '.btn-cancel-add-stage', function (evt) {
-		evt.preventDefault();
-		$(this).closest('tr').remove();
+}
+
+$(document).ready(function () {
+	initializeModelAddStage();
+
+	$('.btn-add-stage').click(function (evt) {	
+		$('#inputAssignment').val($(this).data('assignment'));
 	});
-	
+
 	$('.table-stages').on('click', '.btn-delete-stage', function (evt) {
 		var tr = $(this).closest('tr');
 		var stageId = tr.find('.hidden-stage').val();
-		
+
 		$.confirm({
 			title: 'Delete a Stage',
 			content: 'Please confirm to delete the stage.',
